45e7ee960a0048fb09a9963b1dfbf1fa165c4030
Brice Boutet <boutetlb@gmail.com>
2020-06-01 21:51:21 -0400

    Inquirer code working for one employee

--- Develop/app.js
+++ Develop/app.js
@@ -3,31 +3,86 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
+// const fs = require("fs");​
+// const OUTPUT_DIR = path.resolve(__dirname, "output")
+// const outputPath = path.join(OUTPUT_DIR, "team.html");​
+// const render = require("./lib/htmlRenderer");​​
+// Code to use inquirer to gather information about the development team members,
+
+let membersNumber = 0;
+const optionQuestions = ["Office number:", "GitHub username:", "School"]; //last question by employee category"
+let categoryQuestion = ""; // Selected category last question
+
+
+const questions = [{
+        type: "input",
+        name: "number",
+        message: "How many employees in your team?"
+    },
+    {
+        message: "Select employee type:",
+        name: "category",
+        type: "list",
+        choices: ["Manager", "Employee", "Intern"],
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Enter employee name:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter email address :"
+    },
+];
+
+const finalQuestion = [{
+    type: "input",
+    name: "detail",
+}];
+
+function promptEmployee() {
+    return inquirer.prompt(questions);
+}
+
+function lastPrompt() {
+    return inquirer.prompt(finalQuestion);
+}
+
+async function init() {
+    const answers = await promptEmployee(); // Answers objetc to prompt
+    const category = answers.category;
+    if (category == "Manager") {
+        categoryQuestion = optionQuestions[0];
+    } else if (category == "Employee") {
+        categoryQuestion = optionQuestions[1];
+    } else if (category == "Intern") {
+        categoryQuestion = optionQuestions[2];
+    }
+    finalQuestion[0].message = categoryQuestion;
+    const lastAnswer = await lastPrompt();
+    answers.detail = lastAnswer.detail;
+
+    console.log(answers);
+
+}
+
+init();
+
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
